4e9fdca0d46ec50e9d592b1ec8d7ac7eb9d55976
DESKTOP-JFTTLGG\han <hbg9212@gmail.com>
2023-08-19 09:21:39 +0900

    일부 텍스트 색 변경

--- Program.cs
+++ Program.cs
@@ -2,14 +2,16 @@
 
 namespace spartanDungeon
 {
-    internal class Program
+    class Program
     {
+        public enum ItemTypes { 무기, 방어구, 방패 };
+        public enum Abilitys { 공격력, 방어력 };
         private static Character player;
+
         private static List<Item> myItem = new List<Item>();
 
-        private enum Equipments { 무기, 방어구 };
-        private static Equipments equipment;
-        private static int[] myEquipment = new int[2];
+        private static int[] myAddStat = new int[2];
+        private static int[] myEquipment = new int[3];
 
 
         static void Main(string[] args)
@@ -26,13 +28,25 @@ namespace spartanDungeon
             // 아이템 정보 세팅
             List<ItemAbility> itemAbilities1 = new List<ItemAbility>();
             List<ItemAbility> itemAbilities2 = new List<ItemAbility>();
+            List<ItemAbility> itemAbilities3 = new List<ItemAbility>();
+            List<ItemAbility> itemAbilities4 = new List<ItemAbility>();
 
             itemAbilities1.Add(new ItemAbility(1, 5));
             itemAbilities2.Add(new ItemAbility(0, 2));
-            itemAbilities2.Add(new ItemAbility(1, 2));
+
+            itemAbilities3.Add(new ItemAbility(1, 7));
+            itemAbilities3.Add(new ItemAbility(0, 1));
+
+            itemAbilities4.Add(new ItemAbility(1, 1));
+
             myItem.Add(new Item(1, true, "무쇠갑옷", 1, itemAbilities1, "무쇠로 만들어져 튼튼한 갑옷입니다."));
             myItem.Add(new Item(2, false, "낡은 검", 0, itemAbilities2, "쉽게 볼 수 있는 낡은 검 입니다."));
-            myItem.Add(new Item(3, false, "낡은 검", 0, itemAbilities2, "쉽게 볼 수 있는 낡은 검 입니다."));
+            myItem.Add(new Item(3, true, "가시방패", 2, itemAbilities3, "방어력과 공격력을 동시에 !"));
+            myItem.Add(new Item(4, false, "천 옷", 1, itemAbilities4, "일반적인 천 옷"));
+
+            myEquipment[1] = 1;
+            myEquipment[2] = 3;
+
         }
 
         static void DisplayGameIntro()
@@ -62,19 +76,30 @@ namespace spartanDungeon
 
         static void DisplayMyInfo()
         {
+            AddStat();
             Console.Clear();
-
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("상태보기");
+            Console.ResetColor();
             Console.WriteLine("캐릭터의 정보르 표시합니다.");
             Console.WriteLine();
             Console.WriteLine($"Lv.{player.Level}");
             Console.WriteLine($"{player.Name}({player.Job})");
-            Console.WriteLine($"공격력 :{player.Atk}");
-            Console.WriteLine($"방어력 : {player.Def}");
+            Console.Write($"공격력 : {player.Atk} ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{(myAddStat[0] > 0 ? "(+" + myAddStat[0] + ")" : "")}");
+            Console.ResetColor();
+            Console.Write($"방어력 : {player.Def} ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{(myAddStat[1] > 0 ? "(+" + myAddStat[1] + ")" : "")}");
+            Console.ResetColor();
             Console.WriteLine($"체력 : {player.Hp}");
             Console.WriteLine($"Gold : {player.Gold} G");
             Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("0. 나가기");
+            Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
 
@@ -87,11 +112,36 @@ namespace spartanDungeon
             }
         }
 
+        static void AddStat()
+        {
+            myAddStat[0] = 0;
+            myAddStat[1] = 0;
+            foreach (Item item in myItem)
+            {
+                if (item.Equipment)
+                {
+                    foreach (ItemAbility itemAbility in item.ItemAbilitys)
+                    {
+                        switch (itemAbility.Ability)
+                        {
+                            case Abilitys.공격력:
+                                myAddStat[0] += itemAbility.Stat;
+                                break;
+                            case Abilitys.방어력:
+                                myAddStat[1] += itemAbility.Stat;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
         static void DisplayInventory()
         {
             Console.Clear();
-
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("인벤토리");
+            Console.ResetColor();
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
             Console.WriteLine();
             Console.WriteLine("[아이템 목록]");
@@ -105,25 +155,30 @@ namespace spartanDungeon
                 for (int i = 0; i < str.Length; i++)
                 {
                     // 한글 한글자당 3바이트 씩 사용
-                    if ( Encoding.Default.GetBytes(str.Substring(i,1)).Length == 3 )
+                    if (Encoding.Default.GetBytes(str.Substring(i, 1)).Length == 3)
                         padLen++;
                 }
 
                 padLen = 20 - padLen;
 
+                if (item.Equipment) Console.ForegroundColor = ConsoleColor.Green;
+
                 Console.WriteLine($" - {(item.Equipment ? "[E]" : "   ")}| {str.PadRight(padLen)} | {item.ItemAbilitys[0].Ability} + {item.ItemAbilitys[0].Stat} | {item.Described}");
                 if (item.ItemAbilitys.Count > 1)
                 {
-                    for (int i = 1; i<item.ItemAbilitys.Count; i++)
+                    for (int i = 1; i < item.ItemAbilitys.Count; i++)
                     {
                         string whiteSpace = "";
-                        Console.WriteLine($"{whiteSpace.PadRight(28,' ')} | {item.ItemAbilitys[i].Ability} + {item.ItemAbilitys[i].Stat} |");
+                        Console.WriteLine($"{whiteSpace.PadRight(28, ' ')} | {item.ItemAbilitys[i].Ability} + {item.ItemAbilitys[i].Stat} |");
                     }
                 }
+
+                if (item.Equipment) Console.ResetColor();
             }
-            Console.WriteLine();
             Console.WriteLine("1. 장착 관리");
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("0. 나가기");
+            Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
 
@@ -142,13 +197,15 @@ namespace spartanDungeon
         static void DisplayEquipment(string msg)
         {
             Console.Clear();
-
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("인벤토리 - 장착 관리");
+            Console.ResetColor();
+
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
             Console.WriteLine();
             Console.WriteLine("[아이템 목록]");
             int itemNumber = 1;
-            int itemOver = myItem.Count > 10 ? 1 :0;
+            int itemOver = myItem.Count > 10 ? 1 : 0;
             foreach (Item item in myItem)
             {
 
@@ -164,24 +221,26 @@ namespace spartanDungeon
                 }
 
                 padLen = 20 - padLen;
-
+                if (item.Equipment) Console.ForegroundColor = ConsoleColor.Green;
                 //인벤토리의 아이템이 10개 이상 존재하면, itemNumber 여백 필요
                 //itemOver 변수 사용
                 string strItemNumber = itemNumber + "";
-                Console.WriteLine($" - {strItemNumber.PadRight(1+itemOver)} {(item.Equipment ? "[E]" : "   ")}| {str.PadRight(padLen)} | {item.ItemAbilitys[0].Ability} + {item.ItemAbilitys[0].Stat} | {item.Described}");
+                Console.WriteLine($" - {strItemNumber.PadRight(1 + itemOver)} {(item.Equipment ? "[E]" : "   ")}| {str.PadRight(padLen)} | {item.ItemAbilitys[0].Ability} + {item.ItemAbilitys[0].Stat} | {item.Described}");
                 if (item.ItemAbilitys.Count > 1)
                 {
                     for (int i = 1; i < item.ItemAbilitys.Count; i++)
                     {
                         string whiteSpace = "";
-                        Console.WriteLine($"{whiteSpace.PadRight(30+itemOver, ' ')} | {item.ItemAbilitys[i].Ability} + {item.ItemAbilitys[i].Stat} |");
+                        Console.WriteLine($"{whiteSpace.PadRight(30 + itemOver, ' ')} | {item.ItemAbilitys[i].Ability} + {item.ItemAbilitys[i].Stat} |");
                     }
                 }
-
+                if (item.Equipment) Console.ResetColor();
                 itemNumber++;
             }
             Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("0. 나가기");
+            Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine("원하시는 행동을 입력해주세요.");
             Console.WriteLine(msg);
@@ -200,22 +259,38 @@ namespace spartanDungeon
         static void ChangeEquipment(int index)
         {
             string msg = "";
-            if(myItem[--index].Equipment)
+            --index;
+
+            switch (myItem[index].Type)
             {
-                myItem[index].Equipment = false;
-                myEquipment[myItem[index].Type] = 0;
-            }
-            else
-            {
-                if(myEquipment[myItem[index].Type] == 0)
-                {
-                    myItem[index].Equipment = true;
-                    myEquipment[myItem[index].Type] = myItem[index].ItemId;
-                }
-                else
-                {
-                    msg ="해당 타입의 장비는 이미 착용 중 입니다.";
-                }
+                case ItemTypes.무기:
+                case ItemTypes.방어구:
+                case ItemTypes.방패:
+                    if (myItem[index].Equipment)
+                    {
+                        myItem[index].Equipment = false;
+                        myEquipment[(int)myItem[index].Type] = 0;
+                    }
+                    else
+                    {
+                        if (myEquipment[(int)myItem[index].Type] == 0)
+                        {
+                            myItem[index].Equipment = true;
+
+                            Console.WriteLine(myItem[index].Equipment);
+                            myEquipment[(int)myItem[index].Type] = myItem[index].ItemId;
+                        }
+                        else
+                        {
+                            msg = "이미 착용중인 아이템이 있습니다.";
+                        }
+                    }
+                    break;
+
+                default:
+                    msg = "착용이 불가능한 아이템 입니다.";
+                    break;
+
             }
 
             DisplayEquipment(msg);
@@ -237,70 +312,71 @@ namespace spartanDungeon
                 Console.WriteLine("잘못된 입력입니다.");
             }
         }
-    }
 
 
-    public class Character
-    {
-        public string Name { get; }
-        public string Job { get; }
-        public int Level { get; }
-        public int Atk { get; }
-        public int Def { get; }
-        public int Hp { get; }
-        public int Gold { get; }
 
-        public Character(string name, string job, int level, int atk, int def, int hp, int gold)
+        public class Character
         {
-            Name = name;
-            Job = job;
-            Level = level;
-            Atk = atk;
-            Def = def;
-            Hp = hp;
-            Gold = gold;
-        }
-    }
+            public string Name { get; }
+            public string Job { get; }
+            public int Level { get; }
+            public int Atk { get; }
+            public int Def { get; }
+            public int Hp { get; }
+            public int Gold { get; }
+            public int[] AddStat { get; set; }
 
-    public class Item
-    {
-
-        public int ItemId { get; }
-        public bool Equipment { get; set; }
-        public string ItemName { get; }
-        public int Type { get; } // 0 : 무기, 1 : 방어구
-        public List<ItemAbility> ItemAbilitys { get; }
-        public string Described { get; }
-
-        public Item(int itemId, bool equipment, string itemName, int type, List<ItemAbility> itemAbilitys, string described)
-        {
-            ItemId = itemId;
-            Equipment = equipment;
-            if(itemName.Length > 10)
+            public Character(string name, string job, int level, int atk, int def, int hp, int gold)
             {
-                ItemName = itemName.Substring(0, 10);
+                Name = name;
+                Job = job;
+                Level = level;
+                Atk = atk;
+                Def = def;
+                Hp = hp;
+                Gold = gold;
             }
-            else
-            {
-                ItemName = itemName;
-            }
-            Type = type;
-            ItemAbilitys = itemAbilitys;
-            Described = described;
         }
-    }
 
-    public class ItemAbility
-    {
-        public enum Abilitys { 공격력, 방어력 };
-        public Abilitys Ability { get; }
-        public int Stat { get; }
-
-        public ItemAbility(int ability, int stat)
+        public class Item
         {
-            Ability = (Abilitys)ability;
-            Stat = stat;
+
+            public int ItemId { get; }
+            public bool Equipment { get; set; }
+            public string ItemName { get; }
+            public ItemTypes Type { get; }
+            public List<ItemAbility> ItemAbilitys { get; }
+            public string Described { get; }
+
+            public Item(int itemId, bool equipment, string itemName, int type, List<ItemAbility> itemAbilitys, string described)
+            {
+                ItemId = itemId;
+                Equipment = equipment;
+                if (itemName.Length > 10)
+                {
+                    ItemName = itemName.Substring(0, 10);
+                }
+                else
+                {
+                    ItemName = itemName;
+                }
+                Type = (ItemTypes)type;
+                ItemAbilitys = itemAbilitys;
+                Described = described;
+            }
+        }
+
+        public class ItemAbility
+        {
+
+            public Abilitys Ability { get; }
+            public int Stat { get; }
+
+            public ItemAbility(int ability, int stat)
+            {
+                Ability = (Abilitys)ability;
+                Stat = stat;
+            }
         }
     }
-
 }
